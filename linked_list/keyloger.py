2e0cc9250bc0a974267628dd6fc16884c6bef832
yj <anuefn@naver.com>
2021-04-27 16:19:53 +0900

    add linked_list 2 solutions

---
+++ linked_list/keyloger.py
@@ -0,0 +1,22 @@
+# 5397
+from sys import stdin
+
+n = int(stdin.readline())
+
+for _ in range(n):
+    typing = stdin.readline().strip()
+    left, right = [], []
+    for typ in typing:
+        if typ == '<':
+            if left:
+                right.append(left.pop())
+        elif typ == '>':
+            if right:
+                left.append(right.pop())
+        elif typ == '-':
+            if left:
+                left.pop()
+        else:
+            left.append(typ)
+    left.extend(reversed(right))
+    print(''.join(left))
