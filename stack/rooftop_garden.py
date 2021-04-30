1f4321965be8fbf3c6dd642b744633d95ce4e74d
yj <anuefn@naver.com>
2021-04-30 21:15:28 +0900

    add stack 1 solutions

---
+++ stack/rooftop_garden.py
@@ -0,0 +1,12 @@
+#  6198
+import sys
+
+N = int(sys.stdin.readline())
+li = [int(sys.stdin.readline()) for _ in range(N)]
+stack, res = [], 0
+for i in range(N):
+    while stack != [] and stack[-1] <= li[i]:
+        stack.pop()
+    stack.append(li[i])
+    res += len(stack)-1
+print(res)
