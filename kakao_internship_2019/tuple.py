1304bf9a6dff0c63b4b6ae359642e8e471950ec9
Yxnzoo <anuefn@naver.com>
2021-05-06 18:05:23 +0900

    add 1 solution

---
+++ kakao_internship_2019/tuple.py
@@ -0,0 +1,18 @@
+a = '{{2},{2,1},{2,1,3},{2,1,3,4}}'
+
+
+def solution(s):
+    s = s[2:-2].split('},{')
+    sorted_s = sorted(s, key=lambda x: len(x))
+    answer = []
+    check = set()
+    for value in sorted_s:
+        for each_value in value.split(','):
+            if each_value not in check:
+                answer.append(int(each_value))
+                check.add(each_value)
+                break
+    return answer
+
+
+solution(a)
