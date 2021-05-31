5640e46d0686df89b6f96d2f86ea3bddd5557a40
Yxnzoo <anuefn@naver.com>
2021-05-31 17:31:43 +0900

    add 2 basic solutions

---
+++ basic_code/selfnumber.py
@@ -0,0 +1,12 @@
+#  4763
+nat_num = set(range(1, 10001))
+gen_num = set()
+
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)
+    gen_num.add(i)
+
+self_num = sorted(nat_num - gen_num)
+for i in self_num:
+    print(i)
